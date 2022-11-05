667fa2e907013305ec72fb3a6c8360b4061b440f
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2022-11-05 15:15:29 +0900

    template

---
+++ contest_template.py
@@ -0,0 +1,15 @@
+def input_int():
+    return int(input())
+  
+def input_map_int():
+    return map(int, input().split())
+
+def input_list_int():
+    return list(int, input().split())
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
